52840838accf17667145e2c98635470bd0ed7133
jeettan <jeetta3@gmail.com>
2026-04-26 18:47:01 +0700

    Commit

--- api/index.js
+++ api/index.js
@@ -33,7 +33,7 @@ app.use(session({
 }));
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "dustbin-tracker-app.vercel.app"],
     methods: ["POST", "PUT", "GET", "OPTIONS", "HEAD"],
     credentials: true
 }));
